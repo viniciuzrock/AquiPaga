988304e4311d45d29081786f72e506e431848d2a
viniciuzrock <vinir.santoss@gmail.com>
2023-11-10 17:05:43 -0300

    first commit

---
+++ AquiPaga_API_RESTful/Controllers/TaskController.cs
@@ -0,0 +1,24 @@
+﻿using AquiPaga_API_RESTful.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AquiPaga_API_RESTful.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        [HttpGet]
+        public List<TaskModel> GetAllAsync()
+        {
+            return new List<TaskModel>();
+        }
+
+        [HttpPost]
+        public void PostAsync(TaskModel task)
+        {
+            Console.WriteLine(task);
+            Console.WriteLine(task.Name);
+            Console.WriteLine("Tarefa recebida.");
+        }
+    }
+}
---
+++ AquiPaga_API_RESTful/Models/TaskModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AquiPaga_API_RESTful.Models
+{
+    public class TaskModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Status { get; set; }
+    }
+}
